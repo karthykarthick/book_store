b5aa5047785f9965fd5edc83cb1a72c5b573bb7b
karthick <karthylock@gmail.com>
2021-03-20 00:53:27 +0530

    added educers

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,44 +1,40 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/constants';
 
-const roundID = () => Number((Math.random() * 10000).toFixed(0));
+const roundID = () => Number((Math.random() * 100).toFixed(0));
 
 const bookData = {
   books: [
     {
       id: roundID(),
       title: 'The Lord of the Rings',
-      category: 'Action',
+      category: 'Fiction',
     },
     {
       id: roundID(),
       title: 'Star Wars',
-      category: 'Action',
+      category: 'Fiction',
     },
     {
       id: roundID(),
       title: 'The Witcher',
-      category: 'Action',
+      category: 'Fiction',
     },
     {
       id: roundID(),
       title: 'The Amazing Spider Man',
-      category: 'Sci-Fi',
+      category: 'Comic',
     },
   ],
-  filter: 'All',
 };
 
 const booksReducer = (state = bookData, action) => {
-  const books = state.books; // eslint-disable-line
   switch (action.type) {
     case CREATE_BOOK:
-      if (action.book !== undefined) {
-        books.push(action.book);
-        return ({ books });
-      }
+      // do something here
       break;
     case REMOVE_BOOK:
-      return ({ books: books.filter(book => book.id !== action.book) });
+      // do other thing here
+      break;
     default:
       return state;
   }
